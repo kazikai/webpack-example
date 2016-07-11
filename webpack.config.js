21f138461378304cddf68ce4a51c7d6c7f7a128b
kazikai <kazikai84@gmail.com>
2016-07-11 15:37:40 +0900

    html-webpack-plugin add

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './app.js',
     output: {
-        path: './dist',
+        path: './dist/resources/js/',
         filename: 'app.js'
     },
+    loaders: [
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars'
+      }
+    ],
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             compress: {
@@ -14,5 +21,9 @@ module.exports = {
                 comments: false,
             },
         }),
+        new HtmlWebpackPlugin({
+            title: 'Test App',
+            template: 'docs/index.hbs'
+        })
     ]
 };
