06f64bf893ae9d4fa5189c226b1faf911e94d7fc
한정현(Junghyun Han)/Syrup Store개발팀/SKP <junghyun.han@sk.com>
2016-07-13 17:53:28 +0900

    udpate

--- dist/app.js
+++
@@ -1,1 +0,0 @@
-!function(a){function t(r){if(o[r])return o[r].exports;var e=o[r]={exports:{},id:r,loaded:!1};return a[r].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}var o={};return t.m=a,t.c=o,t.p="",t(0)}([function(a,t,o){var r=o(1);console.log("data",r.data),console.log("data2",r.data2)},function(a,t){var o=["a","b","c","d","e"],r=[1,2,3,4,5];a.exports={data:o,data2:r}}]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
     entry: './app.js',
     output: {
-        path: './dist/resources/js/',
-        filename: 'app.js'
+        path: './dist/',
+        filename: 'resources/js/script.js'
     },
-    loaders: [
-      {
-        test: /\.hbs$/,
-        loader: 'handlebars'
-      }
+    loaders: [ {
+            test: /\.hbs$/,
+            loader: 'handlebars'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        }
     ],
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
@@ -22,8 +26,23 @@ module.exports = {
             },
         }),
         new HtmlWebpackPlugin({
-            title: 'Test App',
-            template: 'docs/index.hbs'
-        })
+            title: '신청화면',
+            template: 'docs/register.hbs',
+            inject: "body",
+            filename: "register.html"
+        }),
+        new HtmlWebpackPlugin({
+            title: '조회화면',
+            template: 'docs/verify.hbs',
+            inject: "body",
+            filename: "verify.html"
+        }),
+        new HtmlWebpackPlugin({
+            title: '승인화면',
+            template: 'docs/manage.hbs',
+            inject: "body",
+            filename: "manage.html"
+        }),
+        new ExtractTextPlugin("styles.css")
     ]
 };
